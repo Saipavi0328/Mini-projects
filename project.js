fe256182389ffb640adf1007cb1cdccfb8b27c7f
Saipavi0328 <saipavithra2803@gmail.com>
2025-05-21 11:25:19 +0530

    first

---
+++ project.js
@@ -0,0 +1,23 @@
+// console.log("Hello World!")
+
+// var message;
+// message="Hello Pavi";
+// console.log(message)
+
+// console.log(document.getElementById("headingElement"));
+
+// document.getElementById("headingElement").textContent="4.0 Technologies";
+// document.getElementById("headingElement").style.color= "blue";
+
+// function display()
+// {
+//     console.log("Hi,this is rahul")
+// }
+
+
+function display()
+{
+    document.getElementById("headingElement").textContent="4.0 Technologies";
+    document.getElementById("headingElement").style.color= "blue";
+
+}
